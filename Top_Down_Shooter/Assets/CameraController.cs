336ac8c317f9f4103b50c52e73aadfc6d4aa86c2
JulianaHansen <juliana.hansen@engenharia.ufjf.br>
2023-11-04 14:16:04 -0300

    tilemap e camera

---
+++ Top_Down_Shooter/Assets/CameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform followTransform;
+
+    void FixedUpdate()
+    {
+        if (followTransform.position.x > -11 && followTransform.position.x < 11)
+        {
+            this.transform.position = new Vector3(followTransform.position.x, this.transform.position.y, this.transform.position.z);
+        }
+        if (followTransform.position.y > -10.8 && followTransform.position.y < 10.8)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, followTransform.position.y, this.transform.position.z);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
